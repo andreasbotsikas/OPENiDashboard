cfcdb65d0f8a206f053c4174f239d1e0297f48f3
Andreas Botsikas <v-abot@microsoft.com>
2014-09-13 15:30:48 +0300

    Added login

---
+++ OPENiDashboard/App/Controllers/RootController.ts
@@ -0,0 +1,10 @@
+﻿class RootController {
+    static $inject = ['$scope'];
+
+    constructor(private $scope) {
+        // Set a reference to the application
+        App.root = this;
+    }
+}
+// Register controller
+App.mod.controller("RootController", RootController); 
---
+++ OPENiDashboard/App/Startup.ts
@@ -0,0 +1,17 @@
+﻿var App = {
+    mod: angular.module('OPENiDashboard', ['ngRoute', 'ngResource']),
+    routes: {},
+    registerRoute: function(path, route) {
+        App.routes[path] = route;
+    },
+    root: null // The root controller
+};
+App.mod.config(['$routeProvider', '$httpProvider', '$locationProvider', function ($routeProvider, $httpProvider, $locationProvider) {
+    for (var path in App.routes) {
+        if (path == "") {
+            $routeProvider.otherwise(App.routes[path]);
+            continue;
+        }
+        $routeProvider.when(path, App.routes[path]);
+    }
+}]); 
