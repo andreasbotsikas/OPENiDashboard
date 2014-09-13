9f5efe39c77d43aeee8d1304c6011a66349bdbee
Andreas Botsikas <v-abot@microsoft.com>
2014-09-13 18:04:36 +0300

    Added media connection and account template

---
+++ OPENiDashboard/App/Controllers/MediaConnect.ts
@@ -0,0 +1,16 @@
+﻿class MediaConnect {
+    static $inject = ['$scope', '$routeParams'];
+    public socialType = "";
+    constructor(private $scope, $routeParams: ng.route.IRouteParamsService) {
+        this.socialType = $routeParams["media"];
+        App.root.currentPage = "Manage " + this.socialType;
+        App.root.currentPageDescription = "Connect/Disconnect account";
+    }
+}
+// Register controller
+App.mod.controller("MediaConnect", MediaConnect);
+App.registerRoute("/media/:media", {
+    templateUrl: 'templates/MediaConnect.html',
+    controller: 'MediaConnect',
+    controllerAs: 'ctr'
+});  
